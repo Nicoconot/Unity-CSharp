2565b6db2c1db910f573e42a9d6b305ddbed540e
Nico de Oliveira Spadoni <nicospadoni1998@gmail.com>
2021-09-09 14:36:45 -0300

    Update and rename FollowCollisionCheck.cs to FollowAI.cs

--- Project-Pigment/FollowAI.cs
+++ Project-Pigment/FollowAI.cs
@@ -1,3 +1,7 @@
+//Simple and effective script! In order to allow the enemy to follow someone within the tilemap, we can't rely on pathfinding as Unity doesn't yet support it for 2D.
+// Instead, we use a big circle collider as a child of the enemy, and whenever the desired object goes within range, the enemy will face and walk in the 
+// direction said object is, in relation to it's current position. It can be expanded to only allow changes in movement if the object is visible, with raycasts.
+
 using UnityEngine;
 
 public class FollowCollisionCheck : MonoBehaviour
