02007c6dfdeed6b6f6419a56ff4f255d171bc89d
Nico de Oliveira Spadoni <nicospadoni1998@gmail.com>
2021-10-07 13:31:00 -0300

    Added sprite switching

--- Project-Pigment/Shooting-Controller.cs
+++ Project-Pigment/Shooting-Controller.cs
@@ -1,7 +1,3 @@
-// Some exciting changes will be made to this code soon. We will stop relying on rotating the gun to face the mouse pointer, and instead 
-// automatically swap out the gun sprites for the one that most accurately points to the cursor, within a list. This will allow us to be truly
-// pixel-perfect. No cheats!
-
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,13 +12,18 @@ public class ShootingController : MonoBehaviour
     public Text debugText;
     public Text colorText;
 
+    private Vector3 mousePos;
 
-    private bool isColiding;
-    [SerializeField]private float previousAngle;
+    public Sprite[] gunSprites;
 
-   
+    public SpriteRenderer gunSr;
+
+    public PlayerSFX sfxController;
+    [SerializeField] private float previousAngle;
+
+
     void Update()
-    {  
+    {
         if (Input.GetButtonDown("Fire1") && !GameManager.gamePaused)
         {
             Shoot();
@@ -34,10 +35,18 @@ public class ShootingController : MonoBehaviour
 
     private void RotateGun()
     {
-        //Here we check the current mouse position and adjust the gun's rotation according to it
+        // This script works in two separate ways. We have an object called "gun" that is invisible in-game and
+        // is simply responsible for placing the gun's tip correctly so we can shoot out of it.
+        // Secondly, we have another object called "gunArm" and its Sprite renderer, which contains 21 different sprites
+        // (x2 for each side) that are swapped depending on the mouse position. This is because we have a pixel
+        // art game, and simply rotating the sprite looks ugly and wrong.
 
-        //Get the Screen positions of the gun object
+
+        //GETTING MOUSE AND PLAYER SCREEN POSITIONS
+
+        //Get the Screen positions of the player object
         Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
 
         //Get the Screen position of the mouse
         Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
@@ -46,23 +55,55 @@ public class ShootingController : MonoBehaviour
         float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
 
 
-
-        debugText.text = angle.ToString();
-        //Ta Daaa
+        //Here we rotate the invisible gun object to change the fireTip's position.
         if (!GameManager.gamePaused) gun.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-    }       
+
+
+        // In order to figure out which sprites to display at which angle, we first must convert the angle variable
+        // into the range of our sprites array. This is done with this formula:
+
+        // NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
+        // Adapted to our needs:
+        // NewValue = ((OldValue +180) * 41) / 360
+
+
+        // Old range: -180 to 180
+        // New range: 1 to (spriterange length, 41)
+
+        int newValue = Mathf.RoundToInt(((angle + 180) * 41) / 360);
+
+
+        gunSr.sprite = gunSprites[newValue];
+
+        debugText.text = newValue.ToString();
+
+        // Some tweaking had to be made to get the sprites order right, but it works perfectly.
+    }
 
     void Shoot()
     {
+        sfxController.PlaySFX("shot");
+
+        // We could use the same vectors in RotateGun() but through game testing we found out that these two work slightly better 
+        // for precise shooting, while the ones in RotateGun() serve their purpose the best
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePos - (Vector2)fireTip.transform.position).normalized;
+
+
+
         //In order to shoot, we first instantiate a bullet at the gun's tip. We then get its rigidbody 
         // and apply force to it.
         GameObject bullet = Instantiate(bulletPrefab, fireTip.position, Quaternion.identity, null);
         Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
-        bulletRb.AddForce(-fireTip.right * bulletSpeed, ForceMode2D.Impulse);
+        //bulletRb.AddForce(-fireTip.transform.right * bulletSpeed);
+        bulletRb.AddForce(direction * bulletSpeed, ForceMode2D.Impulse);
     }
 
     void ChangeGunColors()
     {
+        // For now, we switch the gun's color using the alphanumeric keyboard. I am currently developing a nicer system 
+        // that uses the mouse wheel.
+
         if (!GameManager.gamePaused)
         {
             if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -107,9 +148,10 @@ public class ShootingController : MonoBehaviour
                 gunColor = GunColor.purple;
                 colorText.text = "PURPLE";
                 colorText.color = Colors.purple;
+
             }
         }
-        
+
     }
 
     float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
@@ -117,15 +159,4 @@ public class ShootingController : MonoBehaviour
         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        // I used this for something before, but I can't remember what...
-        // Will probably delete isColiding later
-        isColiding = true;
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        isColiding = false;
-    }
 }
